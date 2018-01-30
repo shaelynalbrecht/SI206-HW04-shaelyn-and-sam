01d9c56153078a8477e089d72f839072391656ca
Shaelyn Albrecht <shaelyn@0587369623.wireless.umich.net>
2018-01-30 15:00:11 -0500

    added checking the question

--- magic_eight.py
+++ magic_eight.py
@@ -1,1 +1,6 @@
 response = raw_input("What is your question? ")
+if response[-1] is not "?":
+    print("I'm sorry, I can only answer questions.")
+
+while response != "quit":
+    response = raw_input("Ask another question: ")
